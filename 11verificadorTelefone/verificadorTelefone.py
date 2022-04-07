e74b05683cf5ee6ce2e71398b36ae298cf709e35
Vediniz <82723849+Vediniz@users.noreply.github.com>
2022-04-07 17:03:25 -0300

    Delete 11verificadorTelefone directory

--- 11verificadorTelefone/verificadorTelefone.py
+++
@@ -1,9 +0,0 @@
-import phonenumbers
-from phonenumbers import geocoder 
-
-numero = input('Digite um número formato: [+55999999999]: ')
-
-telefone = phonenumbers.parse(numero)
-
-print(f'o numero {numero} é da região de', end= " ")
-print(geocoder.description_for_number(telefone, 'pt'))
